96714b5901b3a90b953c1bbccb1e72eb4034c279
Andres Luna <miosesfreddy123@gmail.com>
2025-02-26 12:07:50 +0100

    creacion de 2 clases  producto y cliente  con vistas y pedido  creacion de clase

---
+++ odoo/addons/jahl_tienda_ropa/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import producto
+from . import cliente
+from . import pedido
+
---
+++ odoo/addons/jahl_tienda_ropa/models/producto.py
@@ -0,0 +1,29 @@
+from odoo import models, fields
+from odoo.exceptions import UserError # type: ignore
+
+class Producto(models.Model):
+    _name = 'jahl_tiendaropa.producto'
+    _description = 'Producto de la tienda de ropa'
+
+    # Campos de la clase Producto
+    nombre = fields.Char(string='Nombre', required=True)
+    descripcion = fields.Text(string='Descripción')
+    precio = fields.Float(string='Precio', required=True)
+    stock = fields.Integer(string='Stock', default=0)
+    categoria = fields.Selection(
+        [('camisa', 'Camisa'), ('pantalon', 'Pantalón'), ('zapato', 'Zapato')],
+        string='Categoría', default='camisa'  # Definir una categoría predeterminada
+    )
+
+    # Métodos
+    def actualizar_stock(self, cantidad):
+        """Método para actualizar el stock del producto"""
+        self.stock += cantidad
+
+    def reducir_stock(self, cantidad):
+        """Método para reducir el stock del producto"""
+        if self.stock >= cantidad:
+            self.stock -= cantidad
+        else:
+            raise UserError('No hay suficiente stock para realizar la venta.')
+
